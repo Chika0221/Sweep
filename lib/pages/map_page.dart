fec15b4529ee3894675c139ba40c5ff67979c843
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-05-15 22:23:18 +0900

    mapに機能追加

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+
+// 大津市役所の緯度経度
+const otsuCityOfficePosition = LatLng(35.01889586284015, 135.85529483505871);
 
 class MapPage extends HookConsumerWidget {
   const MapPage({super.key});
@@ -10,6 +14,38 @@ class MapPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final mapController = useState(MapController());
+    final currentLocation = useState(otsuCityOfficePosition);
+
+    Future<void> getcurrentlocation() async {
+      Location location = new Location();
+
+      bool _serviceEnabled;
+      PermissionStatus _permissionGranted;
+      LocationData _locationData;
+
+      _serviceEnabled = await location.serviceEnabled();
+      if (!_serviceEnabled) {
+        _serviceEnabled = await location.requestService();
+        if (!_serviceEnabled) {
+          return;
+        }
+      }
+
+      _permissionGranted = await location.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await location.requestPermission();
+        if (_permissionGranted != PermissionStatus.granted) {
+          return;
+        }
+      }
+
+      final position = await location.getLocation();
+      currentLocation.value = LatLng(position.latitude!, position.longitude!);
+    }
+
+    useEffect(() {
+      getcurrentlocation();
+    }, []);
 
     return Column(
       children: [
@@ -23,10 +59,9 @@ class MapPage extends HookConsumerWidget {
               mapController: mapController.value,
               options: MapOptions(
                 // 龍大瀬田キャンパスの緯度経度
-                initialCenter:
-                    const LatLng(34.96463676923493, 135.94087957022967),
+                initialCenter: otsuCityOfficePosition,
                 // 一倍は世界地図
-                initialZoom: 14.0,
+                initialZoom: 13.0,
                 // 拡大設定
                 maxZoom: 20.0,
                 // 縮小設定
@@ -34,7 +69,6 @@ class MapPage extends HookConsumerWidget {
                 // 初期回転角度
                 initialRotation: 0,
                 onTap: (tapPotision, point) {},
-                
               ),
               children: [
                 TileLayer(
@@ -64,10 +98,12 @@ class MapPage extends HookConsumerWidget {
                   ],
                 ),
                 Positioned(
-                  top: 8,
+                  top: 64,
                   right: 8,
                   child: IconButton.filled(
-                    onPressed: () {},
+                    onPressed: () {
+                      mapController.value.move(currentLocation.value, 15.0);
+                    },
                     icon: Icon(
                       Icons.location_searching_rounded,
                     ),
