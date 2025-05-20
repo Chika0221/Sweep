bdae7c23f0995a7a77bd3cd125eece03d302ec9f
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-05-20 23:53:38 +0900

    ログインのためにfirebaseInitまで実行を確認

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sweep/firebase_options.dart';
 import 'package:sweep/pages/main_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // firebase初期化
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final app = MyApp();
   final providerScope = ProviderScope(child: app);
